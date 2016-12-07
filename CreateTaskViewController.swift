35782370d12762963515e32c8700f443f19ebaee
rparagas <rayparagas@Rays-MacBook-Air.local>
2016-12-07 12:41:52 +1100

    added add task VC elements, created add task cocoa touch file and connected to storyboard, connected elements to code

---
+++ CreateTaskViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CreateTaskViewController.swift
+//  CoreData-DoIt
+//
+//  Created by Ray Paragas on 7/12/16.
+//  Copyright © 2016 Ray Paragas. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
